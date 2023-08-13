d62296aa76ba4bab6ad9dad71575418302e8f907
Faruk Cesur <faruk-cesur@hotmail.com>
2023-08-13 14:50:58 +0300

    Created Fruit, ItemSpawner, ICollectable Scripts

---
+++ Assets/_Project/_Scripts/_Game/Fruit.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Fruit : MonoBehaviour, ICollectable
+{
+    public void Collect()
+    {
+        throw new System.NotImplementedException();
+    }
+}
