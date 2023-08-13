19163d4b3b9e1ff057ef8941fee2cab08f4dd9d4
Faruk Cesur <faruk-cesur@hotmail.com>
2023-08-13 17:55:02 +0300

    Update Health Component

--- Assets/_Project/_Scripts/Utilities/Health.cs
+++ Assets/_Project/_Scripts/Utilities/Health.cs
@@ -1,18 +1,9 @@
-using System;
-using System.Collections;
-using DG.Tweening;
 using UnityEngine;
 using UnityEngine.Events;
-using UnityEngine.UI;
 
 public class Health : MonoBehaviour
 {
     public float StartingHealth { get; set; }
-    public float SliderSpeed = 0.5f;
-    [field: SerializeField] private Slider HealthSlider { get; set; }
-    [field: SerializeField] private Gradient GradientHealthColor { get; set; }
-    [field: SerializeField] private Image Fill { get; set; }
-    [field: SerializeField] private Image HeartImage { get; set; }
     public UnityAction OnDeath;
 
     public bool IsDead { get; set; }
@@ -28,7 +19,6 @@ public class Health : MonoBehaviour
                 return;
 
             _currentHealth = value;
-            SetHealthSliderValue();
 
             if (IsHealthLowerThanZero())
             {
@@ -50,25 +40,6 @@ public class Health : MonoBehaviour
     private void SetStartingHealth()
     {
         CurrentHealth = StartingHealth;
-        HealthSlider.maxValue = StartingHealth;
-        HealthSlider.minValue = 0;
-        SetHealthSliderValue();
-    }
-
-    private void SetHealthSliderValue()
-    {
-        if (HealthSlider.value <= 0)
-            return;
-
-        HealthSlider.DOValue(_currentHealth, SliderSpeed);
-        SetGradientHealthColor();
-    }
-
-    private void SetGradientHealthColor()
-    {
-        var targetHealthSliderValue = (HealthSlider.maxValue - CurrentHealth);
-        var normalizedSliderValue = Mathf.InverseLerp(HealthSlider.maxValue, 0, targetHealthSliderValue);
-        Fill.DOColor(GradientHealthColor.Evaluate(normalizedSliderValue), SliderSpeed);
     }
 
     public void Damage(float damageAmount)
